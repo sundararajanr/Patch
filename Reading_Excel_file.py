a0217e1f5891252af35964883d848e660de5c65f
sundararajanr <rsundarmail77@gmail.com>
2019-01-01 16:11:01 +0530

    Updated file

--- Reading_Excel_file.py
+++ Reading_Excel_file.py
@@ -15,13 +15,13 @@ def create_change(date,desc):
     stream=open('/var/lib/awx/projects/_8__change_creation/change_creation.yml','r')
     data=yaml.load(stream)
 #    print(data)
-    data[0]['tasks'][0]['snow_record']['data']['short_description']=desc
-    data[0]['tasks'][0]['snow_record']['data']['start_date']=date
-    print(data[0]['tasks'][0]['snow_record']['data']['short_description'])
-    print(data[0]['tasks'][0]['snow_record']['data']['start_date'])
-    stream = open('change_creation.yml', 'w')
-    yaml.dump(data,stream, default_flow_style=False)
-#    print(yaml.dump(data))
+    #data[0]['tasks'][0]['snow_record']['data']['short_description']=desc
+    #data[0]['tasks'][0]['snow_record']['data']['start_date']=date
+    #print(data[0]['tasks'][0]['snow_record']['data']['short_description'])
+    #print(data[0]['tasks'][0]['snow_record']['data']['start_date'])
+    #stream = open('change_creation.yml', 'w')
+    #yaml.dump(data,stream, default_flow_style=False)
+#    #print(yaml.dump(data))
 
 
 for x in range(1,user_data.max_row+1):
@@ -29,5 +29,5 @@ for x in range(1,user_data.max_row+1):
     date=str(user_data[x][3].value)
     desc="Linux change for Dec 1/1/2019 Date format updated on 1/1 updated "
     print(date)
-    #create_change(date,desc)
+    create_change(date,desc)
     os.system ('ansible-playbook /var/lib/awx/projects/_8__change_creation/change_creation.yml')
