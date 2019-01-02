19b5e3ecc394148828530c7568a86a52f8244c28
sundararajanr <rsundarmail77@gmail.com>
2019-01-02 11:04:14 +0530

    change creation date updated

--- Reading_Excel_file.py
+++ Reading_Excel_file.py
@@ -13,7 +13,7 @@ try:
 
      #returned_value = os.system(cmd)  # returns the exit code in unix
      #print('returned value:', returned_value)
-     os.system ('ansible-playbook /var/lib/awx/projects/_18__snow_project/Change_creation.yml')
+     os.system ('ansible-playbook /var/lib/awx/projects/_18__snow_project/Change_creation.yml') 
 
 except:
     print("error occured")
