5acbd304c0d03ef8dcafca362c5127af508b345b
sundararajanr <rsundarmail77@gmail.com>
2019-01-02 21:05:12 +0530

    update

--- Reading_Excel_file.py
+++ Reading_Excel_file.py
@@ -13,7 +13,7 @@ try:
      print(os.chdir('/var/lib/awx/projects/_18__snow_project/'))
      print(os.system('ls'))
      print("sundar")
-     os.system ('ansible-playbook Change_creation.yml') 
+     os.system('ansible-playbook Change_cresundaration.yml') 
      #returned_value = os.system(cmd)  # returns the exit code in unix
      #print('returned value:', returned_value)
 
