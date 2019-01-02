01276871419b9b02fab3d4552903ea056a8b5b85
sundararajanr <rsundarmail77@gmail.com>
2019-01-02 21:24:02 +0530

    tested ..

--- Reading_Excel_file.py
+++ Reading_Excel_file.py
@@ -9,11 +9,11 @@ from datetime import datetime
 
 try:
 
-     os.system('ansible-playbook Change_creation.yml') 
+     os.system('/bin/ansible-playbook /var/lib/awx/projects/_18__snow_project/Change_creation.yml') 
      cmd = "git --version"
      print(os.chdir('/var/lib/awx/projects/_18__snow_project/'))
      print(os.system('ls'))
-     print("Krishan")
+     print("testing ...")
      
      #returned_value = os.system(cmd)  # returns the exit code in unix
      #print('returned value:', returned_value)
